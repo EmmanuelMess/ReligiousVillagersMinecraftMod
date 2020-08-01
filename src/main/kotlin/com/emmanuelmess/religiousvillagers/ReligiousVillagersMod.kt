474657dbb012d5e7dd46e825baffaf697244c6ec
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-07-31 22:19:17 -0300

    stuff

--- src/main/kotlin/com/emmanuelmess/religiousvillagers/ReligiousVillagersMod.kt
+++ src/main/kotlin/com/emmanuelmess/religiousvillagers/ReligiousVillagersMod.kt
@@ -1,11 +1,16 @@
 package com.emmanuelmess.religiousvillagers
 
-@Suppress("unused")
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
+
+val FABRIC_ITEM = Item(Item.Settings().group(ItemGroup.MISC))
+
 fun init() {
-    // This code runs as soon as Minecraft is in a mod-load-ready state.
-    // However, some things (like resources) may still be uninitialized.
-    // Proceed with mild caution.
-
-    println("Hello Fabric world!")
+    Registry.register(
+        Registry.ITEM,
+        Identifier("tutorial", "fabric_item"),
+        FABRIC_ITEM
+    )
 }
-
